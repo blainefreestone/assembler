dd4179acdaf785660db17de312e21ae13af6554a
blainefreestone <blainefreestone01@gmail.com>
2024-03-22 13:13:56 -0600

    added optional output file argument in command line and outputs in binary rather than hex now

--- file_assemble.py
+++ file_assemble.py
@@ -1,19 +1,23 @@
 import sys
 from Assembler import Assembler
 
-if len(sys.argv) != 2:
-    print("Usage: py file_assemble.py <input_file>")
+if len(sys.argv) not in range(2, 4):
+    print("Usage: py file_assemble.py <input_file> <output_file>")
     sys.exit(1)
-else:
+elif len(sys.argv) == 2:
     input_file = sys.argv[1]
+    output_file = "output.txt"
+elif len(sys.argv) == 3:
+    input_file = sys.argv[1]
+    output_file = sys.argv[2]
 
 assembler = Assembler()
 
 with open(input_file, 'r') as f:
-    with open("A:\\School\\Winter 2024\\ECEN240\\output.txt", 'w') as output:
-        for line in f:
+    with open(output_file, 'w') as output:
+        for index, line in enumerate(f):
             try:
                 output.write(assembler.assemble(line) + '\n')
             except ValueError as e:
-                print(f"Error: {e}")
+                print(f"Error on line {index + 1}:\n {e}")
                 sys.exit(1)
